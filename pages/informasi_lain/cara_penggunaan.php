6d1fcc3c2e6876900e149acf5e2f61eeb4592deb
adjimuhamadzidan20 <adjimuhamadzidan@gmail.com>
2023-06-29 22:23:06 +0700

    update file
    menambahkan informasi cara penggunaan

--- pages/informasi_lain/cara_penggunaan.php
+++ pages/informasi_lain/cara_penggunaan.php
@@ -1,7 +1,38 @@
-<?php  
-	echo "cara penggunaan";
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-2">
+  <h1 class="h3 text-gray-800">Cara Penggunaan</h1>
+</div>
 
-
-
-
-?>
+<!-- DataTales Example -->
+<div class="card shadow mb-4 rounded-0">
+    <div class="card-body" style="padding: 35px; line-height: 26px;">
+        <h5 class="mb-4">Langkah-langkah penggunaan (diharapkan membaca hingga akhir) :</h5>
+        <div class="text-left"> 
+            <p>
+                <b>Pertama</b>, menambahkan dahulu <b>Data Kriteria</b> dengan memasukan (input) nama kriterianya, kemudian masukkan nilai bobot (untuk nilai bobot dapat ditentukan berdasarkan seberapa kepentingannya), lalu pilih atribut (Benefit/Cost), Benefit yaitu mencari nilai tertinggi (MAX), sedangkan Cost mencari nilai terendah (MIN), setelah diinputkan klik <b>Simpan</b>. Untuk mengisi <b>Data Kriteria</b> ada pada bagian pilihan <b>Menu Data > Data Kriteria</b>.    
+            </p>
+            <p>
+                <b>Kedua</b>, menambahkan <b>Data Alternatif</b> yaitu data-data siswa yang menjadi pilihan / kandidat dalam penentuan keputusan, untuk mengisi <b>Data Alternatif</b> ada pada bagian pilihan <b>Menu Data > Data Alternatif</b>. 
+            </p>
+            <p>
+                <b>Ketiga</b>, menambahkan <b>Sub Kriteria</b>, setelah menginput nama kriteria, dan setiap masing-masing nama kriteria menambahkan sub kriteria dengan mengklik "Masukan Sub Kriteria". Sub kriteria ini bertujuan sebagai pilihan penilaian awal nantinya, untuk mengisi ini ada pada bagian pilihan <b>Menu Data > Data Alternatif</b>..
+            </p>
+            <p>
+                <b>Keempat</b>, menambahkan data <b>Penilaian Awal</b> pada masing-masing setiap data nama alternatif dengan mengklik button 
+                <b>Masukan Penilaian</b>. Terdapat opsi button jika ingin melihat penilaian yang telah di masukan, dengan mengklik button dengan icon mata (eye) yang dimaksud <b>Lihat Penilaian</b>, untuk mengisi ini ada pada bagian pilihan menu <b>Penilaian Awal</b>.  
+            </p>
+            <p>
+                <b>Kelima</b>, setelah menambahkan penilaian, kemudian melakukan <b>Proses Perhitungan</b> untuk membuat SPK atau melakukan proses perhitungan dengan mengklik button <b>Hitung</b>. Untuk melakukan proses ini ada pada bagian pilihan menu <b>Proses Perhitungan</b>. 
+            </p>
+            <p>
+                <b>Keenam</b>, setelah di generate maka akan muncul hasilnya pada bagian <b>Hasil Perhitungan</b>, terdapat 3 buah paginasi yang terletak diatas terdiri yaitu <b>Hasil Normalisasi</b>, <b>Hasil Preferensi</b>, dam <b>Perankingan</b>. Jika ingin me-reset ulang atau menghapus, membersihkan hasil dari perhitungan dapat mengklik button <b>Reset Hasil</b>, maka akan mereset atau menghapus semua hasil perhitungan yang telah dilakukan.
+            </p>
+            <p>
+                <b>Ketujuh</b>, jika ingin mencetak/membuat menjadikan sebuah laporan berbentuk PDF lalu diunduh, bisa memilih pilihan pada menu bagian <b>Laporan</b>, ada 2 pilihan yaitu <b>Cetak Normalisasi</b> dan <b>Cetak Perankingan</b>. Jika mengklik salah satu akan menampilkan tabel data dan terdapat button <b>Cetak Laporan</b>.
+            </p>
+            <p>
+                <b>*Note</b> : Jika ada perubahan pada data baik itu mengedit, menambah data terbaru, menghapus atau apa pun cukup mengklik kembali button <b>Hitung</b> untuk me-generate ulang kembali hasil perhitungan, agar hasil sebelumnya terupdate oleh hasil perhitungan yang terbaru. 
+            </p>
+        </div>
+    </div>
+</div>
--- pages/informasi_lain/tentang_spk.php
+++ pages/informasi_lain/tentang_spk.php
@@ -1,6 +1,10 @@
-<?php  
-	echo "tentang spk";
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-1">
+  <h1 class="h3 text-gray-800">Tentang SPK</h1>
+</div>
 
-
-
-?>
+<!-- DataTales Example -->
+<div class="card shadow mb-4 rounded-0">
+    <div class="card-body">
+    </div>
+</div>
