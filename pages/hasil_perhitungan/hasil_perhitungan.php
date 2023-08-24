014c36161acffa9cc7aab45a57c6d651670ed406
adjimuhamadzidan20 <adjimuhamadzidan@gmail.com>
2023-08-24 09:23:07 +0700

    update file
    update codingan

--- pages/hasil_perhitungan/hasil_perhitungan.php
+++ pages/hasil_perhitungan/hasil_perhitungan.php
@@ -16,16 +16,46 @@
   endif;
 ?>
 
+<?php  
+  if (@$_GET['hasil'] == 'normalisasi') {
+    $activ1 = 'active';
+    $activ2 = '';
+    $activ3 = '';
+    $text1 = 'text-white';
+    $text2 = '';
+    $text3 = '';
+  } else if (@$_GET['hasil'] == 'preferensi') {
+    $activ1 = '';
+    $activ2 = 'active';
+    $activ3 = '';
+    $text1 = '';
+    $text2 = 'text-white';
+    $text3 = '';
+  } else if (@$_GET['hasil'] == 'perankingan') {
+    $activ1 = '';
+    $activ2 = '';
+    $activ3 = 'active';
+    $text1 = '';
+    $text2 = '';
+    $text3 = 'text-white';
+  } else {
+    $activ1 = '';
+    $activ2 = '';
+    $activ3 = '';
+  }
+
+?>
+
 <nav aria-label="...">
   <ul class="pagination pagination-md flex-wrap flex-md-nowrap">
-    <li class="page-item w-100" aria-current="page">
-      <a class="page-link rounded-0 text-gray-800" href="index.php?page=hasil_perhitungan&hasil=normalisasi">Hasil Normalisasi</a>
+    <li class="page-item w-100 <?= $activ1 ?>" aria-current="page">
+      <a class="page-link rounded-0 <?= $text1 ?>" href="index.php?page=hasil_perhitungan&hasil=normalisasi" style="color: grey;">Hasil Normalisasi</a>
     </li>
-    <li class="page-item w-100">
-    	<a class="page-link rounded-0 text-gray-800" href="index.php?page=hasil_perhitungan&hasil=preferensi">Hasil Preferensi</a>
+    <li class="page-item w-100 <?= $activ2 ?>">
+    	<a class="page-link rounded-0 <?= $text2 ?>" href="index.php?page=hasil_perhitungan&hasil=preferensi" style="color: grey;">Hasil Preferensi</a>
     </li>
-    <li class="page-item w-100">
-    	<a class="page-link rounded-0 text-gray-800" href="index.php?page=hasil_perhitungan&hasil=perankingan">Perankingan</a>
+    <li class="page-item w-100 <?= $activ3 ?>">
+    	<a class="page-link rounded-0 <?= $text3 ?>" href="index.php?page=hasil_perhitungan&hasil=perankingan" style="color: grey;">Perankingan</a>
     </li>
   </ul>
 </nav>
--- template/sidebar_menu.php
+++ template/sidebar_menu.php
@@ -1,13 +1,81 @@
+<?php  
+    if (@$_GET['page'] == 'dashboard') {
+        $activ1 = 'active';
+        $activ2 = '';
+        $activ3 = '';
+        $activ4 = '';
+        $activ5 = '';
+        $activ6 = '';
+        $activ7 = '';
+    } else if (@$_GET['page'] == 'menu_data') {
+        $activ1 = '';
+        $activ2 = 'active';
+        $activ3 = '';
+        $activ4 = '';
+        $activ5 = '';
+        $activ6 = '';
+        $activ7 = '';
+    } else if (@$_GET['page'] == 'data_penilaian') {
+        $activ1 = '';
+        $activ2 = '';
+        $activ3 = 'active';
+        $activ4 = '';
+        $activ5 = '';
+        $activ6 = '';
+        $activ7 = ''; 
+    } else if (@$_GET['page'] == 'proses_perhitungan') {
+        $activ1 = '';
+        $activ2 = '';
+        $activ3 = '';
+        $activ4 = 'active';
+        $activ5 = '';
+        $activ6 = '';
+        $activ7 = '';
+    } else if (@$_GET['page'] == 'hasil_perhitungan') {
+        $activ1 = '';
+        $activ2 = '';
+        $activ3 = '';
+        $activ4 = '';
+        $activ5 = 'active';
+        $activ6 = '';
+        $activ7 = '';
+    } else if (@$_GET['page'] == 'laporan') {
+        $activ1 = '';
+        $activ2 = '';
+        $activ3 = '';
+        $activ4 = '';
+        $activ5 = '';
+        $activ6 = 'active';
+        $activ7 = '';
+    } else if (@$_GET['page'] == 'informasi') {
+        $activ1 = '';
+        $activ2 = '';
+        $activ3 = '';
+        $activ4 = '';
+        $activ5 = '';
+        $activ6 = '';
+        $activ7 = 'active';
+    } else {
+        $activ1 = '';
+        $activ2 = '';
+        $activ3 = '';
+        $activ4 = '';
+        $activ5 = '';
+        $activ6 = '';
+        $activ7 = '';
+    }
+?>
+
 <!-- Nav Item - Dashboard -->
-<li class="nav-item active">
+<li class="nav-item <?= $activ1 ?>">
     <a class="nav-link" href="index.php?page=dashboard">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
 </li>
 
 <!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+<li class="nav-item <?= $activ2 ?>">
+    <a class="nav-link collapsed" href="?page=menu_data" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-list-ul"></i>
         <span>Menu Data</span>
@@ -23,7 +91,7 @@
 </li>
 
 <!-- Nav Item - Utilities Collapse Menu -->
-<li class="nav-item">
+<li class="nav-item <?= $activ3 ?>">
     <a class="nav-link collapsed" href="index.php?page=data_penilaian"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw fa-tasks"></i>
@@ -32,7 +100,7 @@
 </li>
 
 <!-- Nav Item - Utilities Collapse Menu -->
-<li class="nav-item">
+<li class="nav-item <?= $activ4 ?>">
     <a class="nav-link collapsed" href="index.php?page=proses_perhitungan"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw fa-calculator"></i>
@@ -41,7 +109,7 @@
 </li>
 
 <!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
+<li class="nav-item <?= $activ5 ?>">
     <a class="nav-link collapsed" href="index.php?page=hasil_perhitungan"
         aria-expanded="true" aria-controls="collapsePages">
         <i class="fas fa-fw fa-table"></i>
@@ -50,8 +118,8 @@
 </li>
 
 <!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePageslaporan"
+<li class="nav-item <?= $activ6 ?>">
+    <a class="nav-link collapsed" href="?page=laporan" data-toggle="collapse" data-target="#collapsePageslaporan"
         aria-expanded="true" aria-controls="collapsePages">
         <i class="fas fa-fw fa-print"></i>
         <span>Laporan</span>
@@ -66,8 +134,8 @@
 </li>
 
 <!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages4"
+<li class="nav-item <?= $activ7 ?>">
+    <a class="nav-link collapsed" href="?page=informasi" data-toggle="collapse" data-target="#collapsePages4"
         aria-expanded="true" aria-controls="collapsePages">
         <i class="fas fa-fw fa-info"></i>
         <span>Informasi Lain</span>
