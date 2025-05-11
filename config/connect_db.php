0de418f63ce9fc4219030eee49d5bd428d6a370d
adjimuhamadzidan20 <adjimuhamadzidan@gmail.com>
2025-05-11 08:43:05 +0700

    update projek

--- config/connect_db.php
+++ config/connect_db.php
@@ -5,6 +5,7 @@
 	$password = '';
 	$database = 'spksiswaprestasi';
 	$koneksi_db = mysqli_connect($host, $username, $password, $database);
+	
 	// cek koneksi db	
 	if (!$koneksi_db) {
 		die('koneksi gagal tersambung '. mysqli_connect_errno());
